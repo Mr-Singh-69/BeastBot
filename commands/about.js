4871d35e55393f26101cb210a1a0eb2129c5e113
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-29 14:46:03 +0530

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,6 +1,11 @@
 module.exports = {
     name: 'about',
     async execute(message) {
-        message.channel.send("Ehsan Quddusi is an Indian politician known for his involvement in various controversies, particularly relating to corruption. He has served as a judge and was a former Orissa High Court judge.");
+        try {
+            message.channel.send("Ehsan Quddusi is an Indian politician known for his involvement in various controversies, particularly relating to corruption. He has served as a judge and was a former Orissa High Court judge.");
+        } catch (error) {
+            console.error(error);
+            message.channel.send('There was an error executing this command!');
+        }
     },
 };
