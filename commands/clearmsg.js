b9c16d432a09fd7cffbf5b926fcc76f2ba1187ef
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-29 14:48:21 +0530

    Update clearmsg.js

--- commands/clearmsg.js
+++ commands/clearmsg.js
@@ -1,22 +1,17 @@
 module.exports = {
     name: 'clearmsg',
     async execute(message, args) {
-        const user = message.mentions.users.first();
-        const deleteCount = parseInt(args[1], 10);
+        try {
+            const user = message.mentions.users.first();
+            const deleteCount = parseInt(args[1], 10);
 
-        if (!user) {
-            return message.channel.send('Please mention a user whose messages you want to delete.');
-        }
+            if (!user) {
+                return message.channel.send('Please mention a user whose messages you want to delete.');
+            }
 
-        if (isNaN(deleteCount) || deleteCount <= 0) {
-            return message.channel.send('Please provide a valid number of messages to delete.');
-        }
+            if (isNaN(deleteCount) || deleteCount <= 0) {
+                return message.channel.send('Please provide a valid number of messages to delete.');
+            }
 
-        const fetchedMessages = await message.channel.messages.fetch({ limit: 100 });
-        const userMessages = fetchedMessages.filter(msg => msg.author.id === user.id).first(deleteCount);
-
-        message.channel.bulkDelete(userMessages)
-            .then(deleted => message.channel.send(`Deleted ${deleted.size} messages from ${user.username}.`))
-            .catch(error => message.channel.send('Error deleting messages. Ensure they are not older than 14 days.'));
-    },
-};
+            const fetchedMessages = await message.channel.messages.fetch({ limit: 100 });
+            const userMessages =
