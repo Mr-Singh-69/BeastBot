dcd57efbc5cb1fb798b0bbfb37ef41ce757ec2b7
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-29 14:47:40 +0530

    Update clearallbot.js

--- commands/clearallbot.js
+++ commands/clearallbot.js
@@ -1,17 +1,22 @@
 module.exports = {
     name: 'clearbot',
     async execute(message, args) {
-        const deleteCount = parseInt(args[0], 10);
+        try {
+            const deleteCount = parseInt(args[0], 10);
 
-        if (isNaN(deleteCount) || deleteCount <= 0) {
-            return message.channel.send('Please provide a valid number of bot messages to delete.');
+            if (isNaN(deleteCount) || deleteCount <= 0) {
+                return message.channel.send('Please provide a valid number of bot messages to delete.');
+            }
+
+            const fetchedMessages = await message.channel.messages.fetch({ limit: 100 });
+            const allBotMessages = fetchedMessages.filter(msg => msg.author.bot).first(deleteCount);
+
+            message.channel.bulkDelete(allBotMessages)
+                .then(deleted => message.channel.send(`Deleted ${deleted.size} bot messages.`))
+                .catch(error => message.channel.send('Error deleting messages. Ensure they are not older than 14 days.'));
+        } catch (error) {
+            console.error(error);
+            message.channel.send('There was an error executing this command!');
         }
-
-        const fetchedMessages = await message.channel.messages.fetch({ limit: 100 });
-        const allBotMessages = fetchedMessages.filter(msg => msg.author.bot).first(deleteCount);
-
-        message.channel.bulkDelete(allBotMessages)
-            .then(deleted => message.channel.send(`Deleted ${deleted.size} bot messages.`))
-            .catch(error => message.channel.send('Error deleting messages. Ensure they are not older than 14 days.'));
     }
 };
